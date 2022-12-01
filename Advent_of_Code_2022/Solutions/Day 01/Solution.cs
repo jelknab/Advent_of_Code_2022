d0152b20c3bb0cf2eb9af67a5bfec1e6959739bd
Jelle Metzlar <github@metzlar.net>
2022-12-01 20:10:49 +0100

    Day 1

--- Advent_of_Code_2022/Solutions/Day 01/Solution.cs
+++ Advent_of_Code_2022/Solutions/Day 01/Solution.cs
@@ -1,14 +1,42 @@
-﻿namespace Advent_of_Code_2022.Solutions.Day_01;
+﻿using System.Text.RegularExpressions;
+
+namespace Advent_of_Code_2022.Solutions.Day_01;
 
 public class Solution: ISolution
 {
+    private static readonly Regex InputRegex = new(@"((?<calories>\d+)\n?)+\n?", RegexOptions.Multiline);
+    
+    public static IEnumerable<IEnumerable<int>> ParseInput(string input)
+    {
+        return InputRegex.Matches(input)
+            .Select(match => match.Groups["calories"].Captures.Select(capture => int.Parse(capture.Value)));
+    }
+
+    public static int FindHighestCalories(IEnumerable<IEnumerable<int>> elfSnacks)
+    {
+        return elfSnacks.Select(snackCalories => snackCalories.Sum()).Max();
+    }
+
+    public static int SumTop3HighestCalories(IEnumerable<IEnumerable<int>> elfSnacks)
+    {
+        return elfSnacks
+            .Select(snackCalories => snackCalories.Sum())
+            .OrderByDescending(totalCalories => totalCalories)
+            .Take(3)
+            .Sum();
+    }
+    
     public string GetFirstAnswer()
     {
-        return "Hello world!";
+        var parsedInput = ParseInput(File.ReadAllText("Solutions/Day 01/input.txt"));
+        
+        return FindHighestCalories(parsedInput).ToString();
     }
 
     public string GetSecondAnswer()
     {
-        throw new NotImplementedException();
+        var parsedInput = ParseInput(File.ReadAllText("Solutions/Day 01/input.txt"));
+        
+        return SumTop3HighestCalories(parsedInput).ToString();
     }
 }
---
+++ Advent_of_Code_2022_tests/Day01/Day01Tests.cs
@@ -0,0 +1,20 @@
+﻿using Advent_of_Code_2022.Solutions.Day_01;
+
+namespace Advent_of_Code_2022_tests.Day01;
+
+public class Day01Tests
+{
+    [Fact]
+    public void ProblemOneExpectedResultTest()
+    {
+        var parsedInput = Solution.ParseInput(File.ReadAllText("Day01/test.input.txt"));
+        Assert.Equal(24000, Solution.FindHighestCalories(parsedInput));
+    }
+    
+    [Fact]
+    public void ProblemTwoExpectedResultTest()
+    {
+        var parsedInput = Solution.ParseInput(File.ReadAllText("Day01/test.input.txt"));
+        Assert.Equal(45000, Solution.SumTop3HighestCalories(parsedInput));
+    }
+}
