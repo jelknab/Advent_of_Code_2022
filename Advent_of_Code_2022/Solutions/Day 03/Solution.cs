ddbe37cfc53fd7e8613d3286294e3bf0fb52408b
Jelle Metzlar <github@metzlar.net>
2022-12-03 15:39:50 +0100

    Solved day 3

---
+++ Advent_of_Code_2022/Solutions/Day 03/Solution.cs
@@ -0,0 +1,74 @@
+﻿namespace Advent_of_Code_2022.Solutions.Day_03;
+
+public class Solution: ISolution
+{
+    public static IEnumerable<string> ParseInput(string input)
+    {
+        return input
+            .Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None)
+            .Where(backpack => !string.IsNullOrEmpty(backpack));
+    }
+
+    public static (string compartmentA, string ComportmentB) GetBackpackCompartments(string backpack)
+    {
+        return (compartmentA: backpack[..(backpack.Length / 2)], ComportmentB: backpack[(backpack.Length / 2)..]);
+    }
+
+    public static IEnumerable<char> FindItemsInBothCompartments(string compartmentA, string comportmentB)
+    {
+        return compartmentA.Intersect(comportmentB);
+    }
+
+    public static IEnumerable<char> FindItemsInAllBackpacks(IEnumerable<string> backpacks)
+    {
+        return backpacks.Select(backpack => backpack.ToCharArray()).Aggregate((a, b) => a.Intersect(b).ToArray());
+    }
+
+    public static int GetItemsPriorityScore(IEnumerable<char> items)
+    {
+        return items
+            .Select(item => item >= 'a' ? item - 'a' + 1 : item - 'A' + 27)
+            .Sum();
+    }
+
+    public static IEnumerable<string[]> GroupBackpacksBy3(IEnumerable<string> backpacks)
+    {
+        using var enumerator = backpacks.GetEnumerator();
+        {
+            while (enumerator.MoveNext())
+            {
+                var items = new string[3];
+
+                for (var i = 0; i < 3; i++)
+                {
+                    items[i] = enumerator.Current;
+                    if (i != 2)
+                    {
+                        enumerator.MoveNext();
+                    }
+                }
+
+                yield return items;
+            }
+        }
+    }
+
+    public string GetFirstAnswer()
+    {
+        return ParseInput(File.ReadAllText("Solutions/Day 03/input.txt"))
+            .Select(GetBackpackCompartments)
+            .Select(compartments => FindItemsInBothCompartments(compartments.compartmentA, compartments.ComportmentB))
+            .Select(GetItemsPriorityScore)
+            .Sum()
+            .ToString();
+    }
+
+    public string GetSecondAnswer()
+    {
+        return GroupBackpacksBy3(ParseInput(File.ReadAllText("Solutions/Day 03/input.txt")))
+            .Select(FindItemsInAllBackpacks)
+            .Select(GetItemsPriorityScore)
+            .Sum()
+            .ToString();
+    }
+}
---
+++ Advent_of_Code_2022_tests/Day03/Day03Tests.cs
@@ -0,0 +1,82 @@
+﻿using Advent_of_Code_2022.Solutions.Day_03;
+
+namespace Advent_of_Code_2022_tests.Day03;
+
+public class Day03Tests
+{
+    [Fact]
+    public void CanSplitCompartments()
+    {
+        var parsedInput = Solution.ParseInput(File.ReadAllText("Day03/test.input.txt")).ToList();
+
+        var compartments = Solution.GetBackpackCompartments(parsedInput.First());
+
+        Assert.Equal("vJrwpWtwJgWr", compartments.compartmentA);
+        Assert.Equal("hcsFMMfFFhFp", compartments.ComportmentB);
+    }
+
+    [Fact]
+    public void FindsSharedItem()
+    {
+        var parsedInput = Solution.ParseInput(File.ReadAllText("Day03/test.input.txt")).ToList();
+
+        var compartments = Solution.GetBackpackCompartments(parsedInput.First());
+        var sharedItems = Solution.FindItemsInBothCompartments(compartments.compartmentA, compartments.ComportmentB);
+
+        Assert.Equal(new[] { 'p' }, sharedItems.ToArray());
+    }
+
+    [Fact]
+    public void ScoresCorrectly()
+    {
+        var parsedInput = Solution.ParseInput(File.ReadAllText("Day03/test.input.txt")).ToList();
+
+        var compartments = Solution.GetBackpackCompartments(parsedInput.First());
+        var sharedItems = Solution.FindItemsInBothCompartments(compartments.compartmentA, compartments.ComportmentB);
+
+        Assert.Equal(16, Solution.GetItemsPriorityScore(sharedItems));
+        Assert.Equal(38, Solution.GetItemsPriorityScore(new []{'L'}));
+        
+        var totalPriorities = parsedInput
+            .Select(Solution.GetBackpackCompartments)
+            .Select(compartments => Solution.FindItemsInBothCompartments(compartments.compartmentA, compartments.ComportmentB))
+            .Select(Solution.GetItemsPriorityScore)
+            .Sum();
+        
+        
+        Assert.Equal(157, totalPriorities);
+    }
+
+    [Fact]
+    public void MakesCorrectGroups()
+    {
+        var parsedInput = Solution.ParseInput(File.ReadAllText("Day03/test.input.txt")).ToList();
+
+        var groups = Solution.GroupBackpacksBy3(parsedInput).ToArray();
+
+        var expectedGroups = new[]
+        {
+            new[] { "vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg" },
+            new[] { "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw" },
+        };
+        
+        Assert.Equal(expectedGroups, groups);
+    }
+
+    [Fact]
+    public void FindsMatchingItems()
+    {
+        var parsedInput = Solution.ParseInput(File.ReadAllText("Day03/test.input.txt")).ToList();
+        var badges = Solution.GroupBackpacksBy3(parsedInput)
+            .Select(backpacks => Solution.FindItemsInAllBackpacks(backpacks).ToArray())
+            .ToArray();
+        
+        var expectedBadges = new[]
+        {
+            new[] { 'r' },
+            new[] { 'Z' },
+        };
+        
+        Assert.Equal(expectedBadges, badges);
+    }
+}
