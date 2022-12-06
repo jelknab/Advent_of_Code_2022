b251b6e4d81772abc1b1156c6a50cde3b7c8f52d
Jelle Metzlar <github@metzlar.net>
2022-12-06 18:46:11 +0100

    Completed day 6

---
+++ Advent_of_Code_2022/Solutions/Day 06/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Advent_of_Code_2022.Solutions.Day_06;
+
+public class Solution: ISolution
+{
+    public static int FindMarkerIndex(string input, int markerLength)
+    {
+        return input.ToCharArray()
+            .RollingWindow(markerLength)
+            .Select((window, index) => (window, index))
+            .First((windowAndIndex) => windowAndIndex.window.Distinct().Count() == markerLength)
+            .index + markerLength;
+    }
+    
+    public string GetFirstAnswer()
+    {
+        return FindMarkerIndex(File.ReadAllText("Solutions/Day 06/input.txt"), 4).ToString();
+    }
+
+    public string GetSecondAnswer()
+    {
+        return FindMarkerIndex(File.ReadAllText("Solutions/Day 06/input.txt"), 14).ToString();
+    }
+}
---
+++ Advent_of_Code_2022_tests/Day06/Day06Tests.cs
@@ -0,0 +1,38 @@
+﻿namespace Advent_of_Code_2022_tests.Day06;
+using Advent_of_Code_2022.Solutions.Day_06;
+
+public class Day06Tests
+{
+    private static readonly string[] TestData = new[]
+    {
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+        "bvwbjplbgvbhsrlpgdmjqwftvncz",
+        "nppdvjthqldpwncqszvftbrmjlhg",
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+    };
+
+    [Fact]
+    public void ProblemOneExpectedOutput()
+    {
+        var indices = TestData.Select(line => Solution.FindMarkerIndex(line, 4)).ToArray();
+        
+        Assert.Equal(7, indices[0]);
+        Assert.Equal(5, indices[1]);
+        Assert.Equal(6, indices[2]);
+        Assert.Equal(10, indices[3]);
+        Assert.Equal(11, indices[4]);
+    }
+    
+    [Fact]
+    public void ProblemTwoExpectedOutput()
+    {
+        var indices = TestData.Select(line => Solution.FindMarkerIndex(line, 14)).ToArray();
+        
+        Assert.Equal(19, indices[0]);
+        Assert.Equal(23, indices[1]);
+        Assert.Equal(23, indices[2]);
+        Assert.Equal(29, indices[3]);
+        Assert.Equal(26, indices[4]);
+    }
+}
